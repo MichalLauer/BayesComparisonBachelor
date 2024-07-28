0c09ea65141afb8614e3350bfde30517df13085c
Michal Lauer <michal.lauer.25@gmail.com>
2024-07-28 10:21:20 +0200

    Working snapshot

--- R/cleanup.R
+++ R/cleanup.R
@@ -1,3 +1,3 @@
 cat("*** Removing .aux files")
-files <- list.files(path = ".", pattern = ".*\\.aux")
+files <- list.files(path = ".", pattern = ".*\\.(aux|lol)$", recursive = T)
 invisible(lapply(files, file.remove))
